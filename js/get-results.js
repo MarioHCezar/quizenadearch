ecb9862639a6bf1cb8c43099c7cb5611d4efac22
Mario <mariohcezar@gmail.com>
2023-02-25 18:33:55 -0300

    Update get-results.js

--- js/get-results.js
+++ js/get-results.js
@@ -1,14 +1,13 @@
-function getResult(element) {
+const getResult = (element) => {
   const id = parseInt(element.id);
   if (id === currentQuestion.answer) {
     element.classList.add("correct");
     updateAnswerIndicator("correct");
-    //console.log("Right!");
     correctAnswers++;
   } else {
     element.classList.add("wrong");
-    //console.log("Try again!")
     updateAnswerIndicator("wrong");
+
     const optionLength = optionContainer.children.length;
     for (let i = 0; i < optionLength; i++) {
       if (parseInt(optionContainer.children[i].id) === currentQuestion.answer) {
@@ -19,35 +18,34 @@ function getResult(element) {
 
   attempt++;
   totalAttempt++;
-  //console.log(optionElement.textContent);
   unclickableOptions();
-}
-function unclickableOptions() {
+};
+
+const unclickableOptions = () => {
   const optionLength = optionContainer.children.length;
   for (let i = 0; i < optionLength; i++) {
     optionContainer.children[i].classList.add("already-answered");
-  }
-}
+  }}
 
-function answersIndicator() {
+const answersIndicator = () => {
   answersIndicatorContainer.textContent = "";
   const numberOfQuestions = filteredQuestions.length;
   for (let i = 0; i < numberOfQuestions; i++) {
     const indicator = document.createElement("div");
     answersIndicatorContainer.appendChild(indicator);
   }
-}
+};
 
-function updateAnswerIndicator(markType) {
+const updateAnswerIndicator = (markType) => {
   answersIndicatorContainer.children[questionCounter - 1].classList.add(
     markType
   );
-}
+};
 
-function next() {
+const next = () => {
   if (questionCounter === filteredQuestions.length) {
     quizOver();
   } else {
     getNewQuestion();
   }
-}
+};
